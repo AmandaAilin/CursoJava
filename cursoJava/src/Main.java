8c17d2c7717ece79bebf206e1609ed3afe6dc672
Amanda Ailin <amandaailindev@gmail.com>
2024-01-05 16:15:06 -0300

    Running Debug

--- cursoJava/src/Main.java
+++ cursoJava/src/Main.java
@@ -20,7 +20,8 @@ public class Main {
 		// EX11();
 		// EX12();
 		// EX13();
-		EX14();
+		// EX14();
+		Terreno();
 	}
 
 	/*
@@ -439,14 +440,11 @@ public class Main {
 	 * Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve pagar de
 	 * Imposto de Renda, segundo a tabela abaixo.
 	 * 
-	 * |-------------------------------|---------------------|
-	 * |            Renda              |  Imposto de Renda   |
-	 * |-------------------------------|---------------------|
-	 * | de 0.0 a R$2000.00            |       Isento        |
-	 * | de R$2000.01 até R$3000.00    |        8%           |
-	 * | de R$3000.01 até R$4500.00    |        18%          |
-	 * | acima de R$4500.00            |        28%          |
-	 * |-----------------------------------------------------| 
+	 * |-------------------------------|---------------------| | Renda | Imposto de
+	 * Renda | |-------------------------------|---------------------| | de 0.0 a
+	 * R$2000.00 | Isento | | de R$2000.01 até R$3000.00 | 8% | | de R$3000.01 até
+	 * R$4500.00 | 18% | | acima de R$4500.00 | 28% |
+	 * |-----------------------------------------------------|
 	 * 
 	 * Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas
 	 * sobre R$ 1000.00, pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00
@@ -458,28 +456,43 @@ public class Main {
 	public static void EX14() {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		double salario = sc.nextDouble();
 		double imposto;
 		if (salario <= 2000.0) {
 			imposto = 0.0;
-		}
-		else if (salario <= 3000.0) {
+		} else if (salario <= 3000.0) {
 			imposto = (salario - 2000.0) * 0.08;
-		}
-		else if (salario <= 4500.0) {
+		} else if (salario <= 4500.0) {
 			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
-		}
-		else {
+		} else {
 			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
 		}
 
 		if (imposto == 0.0) {
 			System.out.println("Isento");
-		}
-		else {
+		} else {
 			System.out.printf("R$ %.2f%n", imposto);
 		}
+
+		sc.close();
+	}
+
+	//Como executar o debug do Eclipse
+	
+	public static void Terreno() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
+		
 		
 		sc.close();
 	}
