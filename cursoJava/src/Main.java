ca880835168fbf47e262368d2e74d4ace14cbd30
Amanda Ailin <amandaailindev@gmail.com>
2024-01-06 00:06:03 -0300

    Repetitive structure (do-while)

--- cursoJava/src/Main.java
+++ cursoJava/src/Main.java
@@ -33,8 +33,9 @@ public class Main {
 		// EX20();
 		// EX21();
 		// EX22();
-		//EX23();
-		EX24();
+		// EX23();
+		//EX24();
+		fahrenheit();
 	}
 
 	/*
@@ -814,19 +815,42 @@ public class Main {
 	 * mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
 	 * exemplo.
 	 */
-	
+
 	public static void EX24() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		
-		for(int i = 1; i<=N; i++) {
+
+		for (int i = 1; i <= N; i++) {
 			int linhas = i;
-			int quadrado = i*i;
-			int cubo = i*i*i;
-			
+			int quadrado = i * i;
+			int cubo = i * i * i;
+
 			System.out.printf("%d %d %d%n", linhas, quadrado, cubo);
 		}
-		
+
 		sc.close();
 	}
+
+	// Estrutura repetitiva "faça-enquanto":
+	/*
+	 * ExFahrenheit: Fazer um programa para ler uma temperatura em Celsius e mostrar o equivalente
+	 * em Fahrenheit. Perguntar se o usuário deseja repetir (s/n). Caso o usuário
+	 * digite "s", repetir o programa. Fórmula: = + 32
+	 */
+
+	public static void fahrenheit() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		char resp;
+		do {
+			System.out.print("Digite a temperatura em Celsius: ");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.out.print("Deseja repetir (s/n)? ");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
+		sc.close();
+	}
+
 }
